af68a829b2a1194e5792514c0f56679c6ea3a6ec
Ștefan Roșu <rosu.stefan@hotmail.com>
2024-09-17 06:58:46 +0000

    Add day-14 higher-lower game

---
+++ day-14-higher-lower-game-project/game_data.py
@@ -0,0 +1,75 @@
+data = [
+    {
+        "name": "Elon Musk",
+        "followers": 150000000,
+        "description": "CEO of SpaceX and Tesla",
+        "country": "USA"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": 250000000,
+        "description": "Professional footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Taylor Swift",
+        "followers": 200000000,
+        "description": "Singer and songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "followers": 220000000,
+        "description": "Actor and former professional wrestler",
+        "country": "USA"
+    },
+    {
+        "name": "Selena Gomez",
+        "followers": 180000000,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Kylie Jenner",
+        "followers": 210000000,
+        "description": "Entrepreneur and media personality",
+        "country": "USA"
+    },
+    {
+        "name": "Lionel Messi",
+        "followers": 230000000,
+        "description": "Professional footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Kim Kardashian",
+        "followers": 190000000,
+        "description": "Media personality and entrepreneur",
+        "country": "USA"
+    },
+    {
+        "name": "Ariana Grande",
+        "followers": 220000000,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Neymar Jr.",
+        "followers": 170000000,
+        "description": "Professional footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Beyoncé",
+        "followers": 160000000,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Justin Bieber",
+        "followers": 200000000,
+        "description": "Singer and songwriter",
+        "country": "Canada"
+    }
+]
+
